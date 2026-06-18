f2a9f7a98bb75cb8085391d9293e108aa1e44d77
payal_domale <payal.domale@caredose.com>
2026-06-18 12:30:49 +0530

    fixed cors

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ const tagRoutes = require("./routes/tagRoutes");
 const postQueryRoutes = require("./routes/postQueryRoutes");
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://your-vercel-app.vercel.app"
+    ],
+    credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.send("API is running successfully 🚀");
